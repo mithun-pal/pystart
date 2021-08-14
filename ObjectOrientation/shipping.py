c88943accbcc86a42cf1985628732e75a60d8243
Mithun Pal <mithunpal989@gmail.com>
2021-08-14 23:21:30 +0530

    added static method

--- ObjectOrientation/shipping.py
+++ ObjectOrientation/shipping.py
@@ -1,8 +1,13 @@
 class ShippingContainer:
     next_serial_num = 1447
 
+    @staticmethod
+    def _generate_serial_num():
+        s_num = ShippingContainer.next_serial_num
+        ShippingContainer.next_serial_num += 1
+        return s_num
+
     def __init__(self, owner_code, contents):
         self.owner_code = owner_code
         self.contents = contents
-        self.serial_num = ShippingContainer.next_serial_num
-        ShippingContainer.next_serial_num += 1
+        self.serial_num = ShippingContainer._generate_serial_num()
