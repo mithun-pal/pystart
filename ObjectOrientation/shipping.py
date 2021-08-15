a119d544e413aa95a6cec7c688159b7a4c7c0eee
Mithun Pal <mithunpal989@gmail.com>
2021-08-15 12:58:33 +0530

    getter setter with property decorator

--- ObjectOrientation/shipping.py
+++ ObjectOrientation/shipping.py
@@ -42,6 +42,32 @@ class RefrigeratedShippingContainer(ShippingContainer):
         self.celsius = celsius
 
     @staticmethod
+    def _c_to_f(celsius):
+        return celsius * 9/5 + 32
+
+    @staticmethod
+    def _f_to_c(fahrenheit):
+        return (fahrenheit - 32) * 5/9
+
+    @property
+    def celsius(self):
+        return self._celsius
+
+    @celsius.setter
+    def celsius(self, tem):
+        if tem > RefrigeratedShippingContainer.MAX_CELSIUS:
+            raise ValueError("Temperature too hot")
+        self._celsius=tem
+
+    @property
+    def fahrenheit(self):
+        return RefrigeratedShippingContainer._c_to_f(self.celsius)
+
+    @fahrenheit.setter
+    def fahrenheit(self, value):
+        self.celsius = RefrigeratedShippingContainer._f_to_c(value)
+
+    @staticmethod
     def _make_bic_code(owner_code, serial):
         return iso6346.create(
             owner_code=owner_code,
