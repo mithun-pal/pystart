8ae6035001547f2c4d70870a66bea1fa918b3a39
Mithun Pal <mithunpal989@gmail.com>
2021-08-15 11:47:36 +0530

    added bic code generation logic

--- ObjectOrientation/shipping.py
+++ ObjectOrientation/shipping.py
@@ -1,13 +1,31 @@
+import iso6346
+
 class ShippingContainer:
     next_serial_num = 1447
 
-    @staticmethod
-    def _generate_serial_num():
-        s_num = ShippingContainer.next_serial_num
-        ShippingContainer.next_serial_num += 1
+    @classmethod
+    def _generate_serial_num(cls):
+        s_num = cls.next_serial_num
+        cls.next_serial_num += 1
         return s_num
+
+    @staticmethod
+    def _make_bic_code(self, owner_code, serial):
+        return iso6346.create(owner_code=owner_code,
+                              serial=str(serial).zfill(6))
+
+    @classmethod
+    def create_empty(cls, owner_code):
+        return cls(owner_code, contents=[])
+
+    @classmethod
+    def create_with_items(cls, owner_code, items):
+        return cls(owner_code, contents=list(items))
 
     def __init__(self, owner_code, contents):
         self.owner_code = owner_code
         self.contents = contents
-        self.serial_num = ShippingContainer._generate_serial_num()
+        self.bic = ShippingContainer._make_bic_code(
+            owner_code=owner_code,
+            serial=ShippingContainer._generate_serial_num()
+            )
